2e971198fdc141c1fcd58848d586ab8bd19e2f0c
mikturnal <mikturnal@posteo.de>
2023-12-04 22:47:07 +0100

    adding solution for day 1 part 1

--- day_1/solution.py
+++ day_1/solution.py
@@ -1,25 +1,18 @@
-def analyse_document(file_path):
+def analyse_document(input_strings):
     combined_digits_list = []
-    written_numbers = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nice"]
 
-    with open(file_path, 'r') as file:
+    with open(input_strings, 'r') as file:
         for line in file:
             found_written_numbers = []
             # Extracting digits from the line
             digits = [int(digit) for digit in line.strip() if digit.isdigit()]
 
-            for substring in written_numbers:
-                if substring in line:
-                    found_written_numbers.append(substring)
-
             # Combining the first and last digits
             if digits:
                 combined_digits = int(str(digits[0]) + str(digits[-1]))
                 combined_digits_list.append(combined_digits)
-            print(found_written_numbers)
     return sum(combined_digits_list)
 
-
-file_path = 'test.txt'
-result = analyse_document(file_path)
-# print(result)
+input_strings = './day_1/input.txt'
+result = analyse_document(input_strings)
+print(result)
